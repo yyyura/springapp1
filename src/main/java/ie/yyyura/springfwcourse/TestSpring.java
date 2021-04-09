015cea18f0f2401a0b33962009b114b44dbfe2d1
Jurijs Cicelimovs <yyyura@gmail.com>
2021-04-09 15:23:53 +0100

    lesson 2

--- src/main/java/ie/yyyura/springfwcourse/TestSpring.java
+++ src/main/java/ie/yyyura/springfwcourse/TestSpring.java
@@ -4,16 +4,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        // we imported in pom.xml spring-context and we have access to this class
+        // lesson 2 -> https://www.youtube.com/watch?v=nLCYk1ySY_U&list=PLAma_mKffTOR5o0WNHnY0mTjKxnCgSXrZ&index=2
+        // we imported in pom.xml "spring-context" and we have access to this class CPXAC "ClassPathXmlApplicationContext"
         // this class read applicationContext.xml and put all beans into Application Context
         // config location in folder mark as resources and same name
-        ClassPathXmlApplicationContext cPxAcontext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext cpxaContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // 1. name of the Bean and 2. we want to get an Object of this class
-        TestBean tb1 = cPxAcontext.getBean("testBean", TestBean.class);
+        TestBean tb1 = cpxaContext.getBean("testBean", TestBean.class);
         System.out.println(tb1.getName());
 
-        cPxAcontext.close();
+        cpxaContext.close();
         /* it will release all the resources and locks that its implementation might hold
         and will also destroy all the cached singleton beans*/
 
